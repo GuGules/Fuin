6b55dc3c35fee6a643d765944318f3b4f0117565
Jules PILLOT <jules.pillot85@gmail.com>
2021-12-03 22:10:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-var ad=2
-function carre(){
-    ad=ad*ad
-    console.log(ad)
+var ad="x"
+function start(){
+    console.log("prout ou crocodile")
+}
+function lol(){
+    if (ad=="prout"){
+        console.log("suce ma bite!")
+    } elif (ad=="crocodile") ;{
+        console.log("bois du viagra!")
+    } else {
+        console.log("tu es obèse")
+    }
 }
